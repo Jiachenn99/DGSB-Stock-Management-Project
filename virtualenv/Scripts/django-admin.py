16600bac8dea472d20aceed7c25de9fa83ca9ab3
JiachennCJC <jiachenn99@gmail.com>
2019-11-06 12:33:41 +0800

    Added venv

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jiachenn\desktop\dgsb-stock-management-project\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
