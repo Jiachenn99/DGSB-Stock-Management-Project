49dc8e5d04a72e9bad20a15878132897aba07d84
JiachennCJC <jiachenn99@gmail.com>
2019-11-11 10:17:11 +0800

    Deleted old virtualenv

--- virtualenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jiachenn\desktop\dgsb-stock-management-project\virtualenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
