2adf897b1a819230dd0bf0144cd5724b5c284786
JiachennCJC <jiachenn99@gmail.com>
2019-11-12 12:48:28 +0800

    Added conditionals for installation

--- setup.bat
+++ setup.bat
@@ -1,6 +1,18 @@
+
 @ECHO OFF
-pip install virtualenv
-virtualenv virtual_environment
-.\virtual_environment\Scripts\activate & pip install -r requirements.txt
-PAUSE
-echo Installation Complete
+
+REM should check whether Python is installed.
+
+REM checking whether pip is installed
+pip -v 
+
+REM install pip if doesnt exist else install virtualenv
+IF ERRORLEVEL 0 (python -m pip install --upgrade pip & pip install virtualenv & echo Installed virtualenv)
+IF ERRORLEVEL 1 (python get-pip.py && python -m pip install --upgrade pip & echo Installed pip)
+
+REM Creating the virtual environment
+virtualenv virtual_environment && .\virtual_environment\Scripts\activate && pip install -r requirements.txt && IF ERRORLEVEL 0 (echo VIRTUAL ENVIRONMENT SUCCESSFULLY CREATED & echo REQUIREMENTS INSTALLED)
+
+
+
+
