f1f3743b6e828a3a15d11577b17743bb830b14f2
JiachennCJC <jiachenn99@gmail.com>
2020-03-02 23:34:07 +0800

    Testing

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('index/', views.index,name='index'),
-    path('register/', views.register, name='register'),
     path('irrigation/', views.irrigation, name='irrigation'),
     path('plantation/', views.plantation, name='plantation'),
     path('vehicles/', views.vehicles, name='vehicles'),
+    path('testing/', views.get_name, name='get_name')
 ]
