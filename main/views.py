59ab11a30a0e38a39d867f9e76bbd5c177ddf961
JiachennCJC <jiachenn99@gmail.com>
2020-03-25 22:42:19 +0800

    Modified result fetching to match new query_functions.py

--- main/views.py
+++ main/views.py
@@ -3,18 +3,11 @@ from django.http import HttpResponseRedirect
 from main.forms import *
 from main.models import *
 from main.query_functions import *
-<<<<<<< HEAD
-import MySQLdb
-
-db = MySQLdb.connect(host="localhost",user="root", db="duriangarden", port = 3306)
-results_list = []
-headers_list = []
-=======
 from main.get_data import *
 import MySQLdb
 
+results_list = []
 db = MySQLdb.connect(host="localhost",user="root", db="duriangarden", port = 3306)
->>>>>>> dynamic_tables
 
 def dashboard(request):
     context = {"dashboard": "active"}
@@ -31,9 +24,9 @@ def register(request):
 
 def purchases(request):
 
-    results_list, header_list = get_all_results(Purchasing)
+    results_list = get_all_results(Purchasing)
 
-    context = {"purchases": "active",'result': results_list, 'headers_list': header_list}
+    context = {"purchases": "active",'result': results_list}
     return render(request, 'main/purchases.html',context)
 
 def order(request):
@@ -42,38 +35,23 @@ def order(request):
     return render(request, 'main/order.html',context)
   
 def irrigation(request):
-<<<<<<< HEAD
 
-    results_list, header_list = get_all_results(Irrigation)
-
-    context = {"irrigation": "active", 'result': results_list, 'headers_list': header_list}
-    return render(request, 'main/irrigation.html',context)
-
-def plantation(request):
-
-    # results_list, header_list = get_all_results(Plantation)
-
-    context = {"plantation": "active"}
-    return render(request, 'main/plantation.html',context)
-
-def vehicles(request):
-
-    results_list, header_list = get_all_results(Vehicle) 
-
-    context = {"vehicles": "active",'result': results_list, 'headers_list': header_list}
-    return render(request, 'main/vehicles.html',context)
-=======
-    context = get_irrigation()
-    return render(request, 'main/irrigation.html', context)
+    results = get_all_results(Irrigation)
+    cat_list = ['Irrigation']
+    context = {'results': results, 'cat_list': cat_list, 'label':"Irrigation"}
+    return render(request, 'main/irrigation_2.html', context)
 
 def plantation(request):
-    context = get_plantation()
+    
+    results = get_plantation()
+    cat_list = ['Tools','Fungicide','Consumables']
+
+    context = {'results': results,'cat_list': cat_list, 'label':"Plantation"}
     return render(request, 'main/plantation.html',context)
 
 def vehicle(request):
     context = get_vehicle()
     return render(request, 'main/vehicle.html',context)
->>>>>>> dynamic_tables
 
 def addItem(request, form_name):
     # Create and update database
@@ -112,29 +90,4 @@ def findForm(form_type):
         'Pesticide' : PesticideForm,
     }
     return switch.get(form_type)
-
-
-def get_name(request):
-
-    # Initializing the form
-    form2 = HerbicideForm
-    # form_class = NameForm
-    # form = form_class(request.POST or None)
-    header_list = []
-    results_list = []
-
-    if request.method == "POST":
-        if form2.is_valid:
-            return HttpResponseRedirect('/thanks/')
-
-    c = db.cursor()
-    # addItem(request, Herbicide)
-
-    results_list, header_list = get_all_results(Herbicide)
-
-    # model_fields = Tools._meta.fields
-    render_dict = {'result': results_list,'form':form2, 'header_list': header_list}
-
-    return render(request,'main/testing.html',render_dict)
-
 
