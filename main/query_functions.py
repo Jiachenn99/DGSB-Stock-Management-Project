a70142ba175df5755bd96ed83b747d7bb9aaf288
Ze-Cong <jacksonlzc14@gmail.com>
2020-05-01 14:08:05 +0800

    Fixed paginator crash on 'previous' and 'next' button

--- main/query_functions.py
+++ main/query_functions.py
@@ -139,14 +139,14 @@ def get_supplier_name(subcategory, some_queryset):
             if 'pv_no' in dicts.keys():
                 model_object = subcategory.objects.get(pk = dicts['purchasing_id'])
                 supplier_name = model_object.supplier.supplier_name
-                del[dicts['supplier_id']]
-                dicts['supplier_name'] = supplier_name
+                dicts['supplier_id'] = supplier_name
+                
  
             else:
                 model_object = subcategory.objects.get(pk = dicts['id'])
                 supplier_name = model_object.purchasing.supplier.supplier_name
-                del[dicts['purchasing_id']]
-                dicts['supplier_name'] = supplier_name
+                dicts['purchasing_id'] = supplier_name
+                
             
     return some_queryset        
 
