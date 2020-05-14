db8f1c9373c9e0cb1bd76e25d72de3ee77acd5ec
JiachennCJC <33684863+Jiachenn99@users.noreply.github.com>
2020-05-14 10:48:24 +0800

    Update windows_setup.bat

--- Setup/windows_setup.bat
+++ Setup/windows_setup.bat
@@ -7,8 +7,8 @@ REM checking whether pip is installed
 pip --version
 
 REM install pip if doesnt exist else install virtualenv
-IF ERRORLEVEL 0 (py -m pip install --upgrade pip & pip install virtualenv & echo Installed virtualenv)
-IF ERRORLEVEL 1 (py get-pip.py && py -m pip install --upgrade pip & echo Installed pip)
+IF ERRORLEVEL 0 (python -m pip install --upgrade pip & pip install virtualenv & echo Installed virtualenv)
+IF ERRORLEVEL 1 (python get-pip.py && py -m pip install --upgrade pip & echo Installed pip)
 
 REM Creating the virtual environment
 virtualenv ../virtual_environment && ..\virtual_environment\Scripts\activate && pip install -r requirements.txt && IF ERRORLEVEL 0 (echo VIRTUAL ENVIRONMENT SUCCESSFULLY CREATED & echo REQUIREMENTS INSTALLED)
